90f29a1c793c612cfa48b4a8e1e911d8e4498da6
Gabriel Costa Rabelo <gabriel.costa.rabelo@gmail.com>
2022-12-08 23:32:32 -0300

    add new information

--- UC10-interação-com-apis/src/app/app.module.ts
+++ UC10-interação-com-apis/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { ContatoComponent } from './views/contato/contato.component';
 import { HeaderComponent } from './views/header/header.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContatoComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
